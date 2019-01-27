1d8ad1089d42ed1caeeea4597b78cd74000c587c
Angela Malone <a.malone1193@gmail.com>
2019-01-27 16:39:32 -0500

    search

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,45 +3,39 @@ import Link from "./Link";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-class LinkList extends Component {
-  render() {
-    const FEED_QUERY = gql`
-      {
-        feed {
-          links {
+export const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
             id
-            createdAt
-            url
-            description
-            postedBy {
-              id
-              name
-            }
-            votes {
-              id
-              user {
-                id
-              }
-            }
           }
         }
       }
-    `;
+    }
+  }
+`;
 
-    const linksToRender = [
-      {
-        id: "1",
-        description:
-          "Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎",
-        url: "https://www.prisma.io"
-      },
-      {
-        id: "2",
-        description: "The best GraphQL client",
-        url: "https://www.apollographql.com/docs/react/"
-      }
-    ];
+class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
 
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+  render() {
     return (
       <Query query={FEED_QUERY}>
         {({ loading, error, data }) => {
@@ -53,7 +47,12 @@ class LinkList extends Component {
           return (
             <div>
               {linksToRender.map((link, index) => (
-                <Link key={link.id} link={link} index={index} />
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
               ))}
             </div>
           );
