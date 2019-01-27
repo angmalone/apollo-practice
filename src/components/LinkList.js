cd8b94843ae7438b37fb8f4ea035a19909e682e3
Angela Malone <a.malone1193@gmail.com>
2019-01-27 16:20:55 -0500

    voting + auth

---
+++ src/components/LinkList.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Link from "./Link";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+class LinkList extends Component {
+  render() {
+    const FEED_QUERY = gql`
+      {
+        feed {
+          links {
+            id
+            createdAt
+            url
+            description
+            postedBy {
+              id
+              name
+            }
+            votes {
+              id
+              user {
+                id
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const linksToRender = [
+      {
+        id: "1",
+        description:
+          "Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎",
+        url: "https://www.prisma.io"
+      },
+      {
+        id: "2",
+        description: "The best GraphQL client",
+        url: "https://www.apollographql.com/docs/react/"
+      }
+    ];
+
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default LinkList;
